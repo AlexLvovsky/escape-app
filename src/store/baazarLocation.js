ec96b0dca33b7bce5e3c1d854fb441fe855018b0
Maryana <labgarmar@gmail.com>
2024-03-09 11:25:51 +0200

    version3

---
+++ src/store/baazarLocation.js
@@ -0,0 +1,119 @@
+export const baazarLoccation = {
+  puzzleAboutLoccation: {
+    id: 1,
+    puzzleText: `Они стали важными факторами развития городов и торговли в Европе в средние века. 
+    Они способствовали распространению товаров, идей и культуры, 
+    а также стимулировали развитие ремесла и торговли. Что же это - «они»?`,
+    puzzleFilePath: ``,
+    video: '',
+    discription_text: ``,
+    discription_filePath: ``,
+    answer: '',
+    rightAnswer: ['Ярмарки', 'Ярмарка', 'Рынки', 'базар'],
+    done: false,
+    last_discription_text: `Ярмарки в прошлом были значимыми событиями, 
+    проводимыми один или несколько раз в году в различных городах. Сейчас же они могут устраиваться 
+    даже раз в неделю, что, возможно, свидетельствует о потребности людей в более частых 
+    встречах — очередное отличие людей от ИИ, которому встречи абсолютно не нужны.<br />
+
+    Направляйтесь на место, где в Куинстауне проходят ярмарки каждую субботу. 
+    По информации от спутников Space X, капитан команды также был замечен на этом мероприятии.`,
+    last_discription_filePath: `/locationBaazar/part-1.mp3`,
+    clues: [
+      {
+        id: '1',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+      {
+        id: '2',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+      {
+        id: '3',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+    ],
+  },
+  QRCode: {
+    filePath: ``,
+  },
+  puzzlePrevMain: {
+    id: 1,
+    puzzleText: `Посчитайте колышки вокруг березы, ну или ивы. Сами решайте, что это такое. Сколько их?`,
+    puzzleFilePath: ``,
+    video: '',
+    discription_text: ``,
+    discription_filePath: ``,
+    answer: '',
+    rightAnswer: ['9'],
+    done: false,
+    last_discription_text: ``,
+    last_discription_filePath: ``,
+    clues: [
+      {
+        id: '1',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+      {
+        id: '2',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+      {
+        id: '3',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+    ],
+  },
+  puzzleMain: {
+    id: 1,
+    puzzleText: `ИИ никогда не сможет создать такое же кино, как человек. 
+    Искусство созданное человеком совершенно, но не идеально на 100%, и именно этим прекрасно. 
+    Такое неидеальное искусство мы, люди, называем живым. Вспомните 2000-е… Времена когда на 
+    экранах древние символы раскрывали свои тайны, а ии создавал тысячи актеров с их собственным 
+    интеллектом, пользователи портативных артефактов в реальной жизни применяли уникальные ритуальные 
+    последовательности движений, создавая свой собственный код общения. <br /> Назовите зашифрованный фильм: 
+    <br /> <b>"242663445 45437"</b>`,
+    puzzleFilePath: ``,
+    video: '',
+    discription_text: ``,
+    discription_filePath: ``,
+    answer: '',
+    rightAnswer: ['Властелин Колец'],
+    done: false,
+    last_discription_text: `Программный пакет назывался MASSIVE, 
+    а использовался он для создания боевых сцен в фильмах «Властелин колец».`,
+    last_discription_filePath: `/locationBaazar/part-2.mp3`,
+    clues: [
+      {
+        id: '1',
+        used: false,
+        text: 'Какими технологиями отличались 2000-е года от наших?',
+        filePath: '',
+      },
+      {
+        id: '2',
+        used: false,
+        text: 'Разница в смартфонах и кнопочных телефонах',
+        filePath: '',
+      },
+      {
+        id: '3',
+        used: false,
+        text: 'here must be picture of phonekeybord', //need to complite
+        filePath: '',
+      },
+    ],
+  },
+};
--- src/store/courtLocation.js
+++ src/store/courtLocation.js
@@ -1,115 +1,117 @@
-export const puzzleAboutLoccation = {
-  id: 1,
-  puzzleText: `Следующая локация связана со спортом. Первый мяч для этой игры был сделан из дерева. 
+export const courtLocation = {
+  puzzleAboutLoccation: {
+    id: 1,
+    puzzleText: `Следующая локация связана со спортом. Первый мяч для этой игры был сделан из дерева. 
     Назовите этот спорт`,
-  puzzleFilePath: ``,
-  video: '',
-  discription_text: ``,
-  discription_filePath: ``,
-  answer: '',
-  rightAnswer: ['теннис'],
-  done: false,
-  last_discription_text: `В начале истории тенниса использовались деревянные мячи. 
+    puzzleFilePath: ``,
+    video: '',
+    discription_text: ``,
+    discription_filePath: ``,
+    answer: '',
+    rightAnswer: ['теннис'],
+    done: false,
+    last_discription_text: `В начале истории тенниса использовались деревянные мячи. 
     Современный теннисный мяч изготавливается из каучука. 
     Ваше следующее задание находится здесь.`,
-  last_discription_filePath: `/locationCourt/part-1.mp3`,
-  clues: [
-    {
-      id: '1',
-      used: false,
-      text: '',
-      filePath: '',
-    },
-    {
-      id: '2',
-      used: false,
-      text: '',
-      filePath: '',
-    },
-    {
-      id: '3',
-      used: false,
-      text: '',
-      filePath: '',
-    },
-  ],
-};
-export const QRCode = {
-  filePath: ``,
-};
-export const puzzlePrevMain = {
-  id: 1,
-  puzzleText: `Поздравляем! Вы прибыли на локацию номер два. 
+    last_discription_filePath: `/locationCourt/part-1.mp3`,
+    clues: [
+      {
+        id: '1',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+      {
+        id: '2',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+      {
+        id: '3',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+    ],
+  },
+  QRCode: {
+    filePath: ``,
+  },
+  puzzlePrevMain: {
+    id: 1,
+    puzzleText: `Поздравляем! Вы прибыли на локацию номер два. 
     Посчитайте секции забора со стороны парковки. Сколько их?`,
-  puzzleFilePath: ``,
-  video: '',
-  discription_text: ``,
-  discription_filePath: ``,
-  answer: '',
-  rightAnswer: ['12'],
-  done: false,
-  last_discription_text: ``,
-  last_discription_filePath: ``,
-  clues: [
-    {
-      id: '1',
-      used: false,
-      text: '',
-      filePath: '',
-    },
-    {
-      id: '2',
-      used: false,
-      text: '',
-      filePath: '',
-    },
-    {
-      id: '3',
-      used: false,
-      text: '',
-      filePath: '',
-    },
-  ],
-};
-export const puzzleMain = {
-  id: 1,
-  puzzleText: `Именно самосознание помогает людям заботиться не только о себе 
+    puzzleFilePath: ``,
+    video: '',
+    discription_text: ``,
+    discription_filePath: ``,
+    answer: '',
+    rightAnswer: ['12'],
+    done: false,
+    last_discription_text: ``,
+    last_discription_filePath: ``,
+    clues: [
+      {
+        id: '1',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+      {
+        id: '2',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+      {
+        id: '3',
+        used: false,
+        text: '',
+        filePath: '',
+      },
+    ],
+  },
+  puzzleMain: {
+    id: 1,
+    puzzleText: `Именно самосознание помогает людям заботиться не только о себе 
     но и помогать людям. Как ИИ мог захватить мир? 
     Это же только инструмент, с помощью которго человек достигает самых смелых поставленных целей. 
     Например, в 1965 году была написана одна из первых программ искусственного интеллекта. 
     Расшифруйте имя и фамилию создателя этой программы. 
     Кстати, к Цезарю это задание имеет прямое отношение. <br /><br />
     <b>«Нгуо Жйзугффл» - ?</b>`,
-  puzzleFilePath: ``,
-  video: '',
-  discription_text: ``,
-  discription_filePath: ``,
-  answer: '',
-  rightAnswer: ['Карл Джерасси'],
-  done: false,
-  last_discription_text: `Карл Джерасси - это американский ученый, 
+    puzzleFilePath: ``,
+    video: '',
+    discription_text: ``,
+    discription_filePath: ``,
+    answer: '',
+    rightAnswer: ['Карл Джерасси'],
+    done: false,
+    last_discription_text: `Карл Джерасси - это американский ученый, 
   который разработал программу DENDRAL, она могла автоматически обнаруживать новые формы лекарств. 
   Кстати, этот же ученый разработал таблетку контроля над рождаемостью.”`,
-  last_discription_filePath: `/locationCourt/part-2.mp3`,
-  clues: [
-    {
-      id: '1',
-      used: false,
-      text: `«Егр Ёсё» - Ван Гог<br />«Грхсрлс Елегояжл» - Антонио Вивальди`,
-      filePath: '',
-    },
-    {
-      id: '2',
-      used: false,
-      text: `Каждая буква сдвинута на определенное количество букв в алфавите.
+    last_discription_filePath: `/locationCourt/part-2.mp3`,
+    clues: [
+      {
+        id: '1',
+        used: false,
+        text: `«Егр Ёсё» - Ван Гог<br />«Грхсрлс Елегояжл» - Антонио Вивальди`,
+        filePath: '',
+      },
+      {
+        id: '2',
+        used: false,
+        text: `Каждая буква сдвинута на определенное количество букв в алфавите.
        Открой алфавит и посчитай на сколько.`,
-      filePath: '',
-    },
-    {
-      id: '3',
-      used: false,
-      text: `Каждая буква сдвинута на 3 буквы алфавита вперед`,
-      filePath: '',
-    },
-  ],
+        filePath: '',
+      },
+      {
+        id: '3',
+        used: false,
+        text: `Каждая буква сдвинута на 3 буквы алфавита вперед`,
+        filePath: '',
+      },
+    ],
+  },
 };
